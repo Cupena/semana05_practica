b19de51c7efdbc29585846e5846535f2e4f7603d
cupena <cupena@uamv.edu.ni>
2026-09-16 08:31:19 -0600

    Adicion de correccion de errores

--- registro.py
+++ registro.py
@@ -2,20 +2,27 @@
 
 datos = []
 def registrar_datos(datos):
-    def error():
-        print("Ingrese  ")
     estudiante = input("Nombre de estudiante: ")
+    def error():
+        print("Ingrese un valor valido")
 
-    try:
-        nota1 = int(input("Ingrese la nota 1:"))
-    except ValueError:
-        error()
-    try:
-        nota2 = int(input("Ingrese la nota 2:"))
-    except ValueError:
-        error()
-    try:
-        nota3 = int(input("Ingrese la nota 3:"))
-    except ValueError:
-        error()
+    while True:
+        try:
+            nota1 = int(input("Ingrese la nota 1:"))
+            break
+        except ValueError:
+            error()
+    while True:
+        try:
+            nota2 = int(input("Ingrese la nota 2:"))
+            break
+        except ValueError:
+            error()
+    while True:
+        try:
+            nota3 = int(input("Ingrese la nota 3:"))
+            break
+        except ValueError:
+            error()
+
     datos.append([estudiante, [nota1, nota2, nota3]])
