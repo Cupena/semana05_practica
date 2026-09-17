25287f1a1719623f2e3d2dd803c3589e9bba36bd
Luis Carlos Rodriguez Rodriguez <lcrodriguez@uamv.edu.ni>
2026-09-17 10:17:14 -0600

    Update mostrarDatos.py

--- mostrarDatos.py
+++ mostrarDatos.py
@@ -1,4 +1,11 @@
-#Mostrar datos
-from registro import datos
+def mostrar_datos(datos):
+    if not datos:
+        print("No hay estudiantes registrados.")
+        return
 
-print(datos)
+    print("Datos de los estudiantes:")
+    for alumno in datos:
+        nombre = alumno[0]
+        notas = alumno[1]
+
+        promedio = sum(notas) / len(notas)
