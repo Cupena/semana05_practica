cab81e1b5764ac13a927af2a6ec695e6f754f21d
Carlos P <cupena@uamv.edu.ni>
2026-09-17 23:49:07 -0600

    fase final

--- registro.py
+++ registro.py
@@ -7,22 +7,29 @@ def registrar_datos(datos):
 
     while True:
         try:
-            nota1 = int(input("Ingrese la nota 1:"))
-            if nota1 < 0:
+            nota1 = int(input("Ingrese la nota 1: "))
+            if nota1 < 0 or nota1 > 100:
                 error()
-            break
+            else:
+                break
         except ValueError:
             error()
     while True:
         try:
-            nota2 = int(input("Ingrese la nota 2:"))
-            break
+            nota2 = int(input("\nIngrese la nota 2: "))
+            if nota2 < 0 or nota2 > 100:
+                error()
+            else:
+                break
         except ValueError:
             error()
     while True:
         try:
-            nota3 = int(input("Ingrese la nota 3:"))
-            break
+            nota3 = int(input("\nIngrese la nota 3: "))
+            if nota3 < 0 or nota3 > 100:
+                error()
+            else: 
+                break
         except ValueError:
             error()
 
